9deb07bc374a2a518e28efbade5e935dc734795e
Bart Reardon <bart.reardon@csiro.au>
2015-06-17 17:19:53 +1000

    Update adgroups.sh
    bit of a cleanup
    
    alternate way to get the current console user

--- adgroups.sh
+++ adgroups.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
-# custom user-related conditions:
 # console_user_ad_group_member_of: list of AD groups the current console user is a member of
 
 DEFAULTS=/usr/bin/defaults
 MUNKI_DIR=$($DEFAULTS read /Library/Preferences/ManagedInstalls ManagedInstallDir)
 COND_DOMAIN="$MUNKI_DIR/ConditionalItems"
-NET_DOMAIN=$(dsconfigad -show | grep "Directory Domain" | awk -F"= " '{print $2}' | awk -F"." '{print $1}' | tr [a-z] [A-Z])
+AD_DOMAIN=$(dsconfigad -show | grep "Directory Domain" | awk -F"= " '{print $2}' | awk -F"." '{print $1}' | tr [a-z] [A-Z])
 
-CONSOLE_USER=$(who | grep console | grep -v _ | awk '{print $1}')
-USER_AD_GROUPS=$(dscl /Active\ Directory/$NET_DOMAIN/All\ Domains -read /Users/$CONSOLE_USER dsAttrTypeNative:memberOf | sed -e 's/.*CN=//g;s/,OU=.*//g')
+CONSOLE_USER=$(who | grep console | cut -d ' ' -f1)
+USER_AD_GROUPS=$(dscl /Active\ Directory/$AD_DOMAIN/All\ Domains -read /Users/$CONSOLE_USER dsAttrTypeNative:memberOf | sed -e 's/.*CN=//g;s/,OU=.*//g')
 IFS=$'\n'
 
 for GROUP_NAME in $USER_AD_GROUPS; do

70e83ff730b56f87efda286e1162a5a5196a32fb
Bart Reardon <bart.reardon@gmail.com>
2015-06-19 22:52:10 +1000

    cleanup, added check for group list file, moved things around to be a little more efficient

--- group_check.sh
+++ group_check.sh
@@ -1,40 +1,43 @@
 #!/bin/sh
 
-# security groups to check
-#declare -a GROUP_ARRAY=("ACL_SCCM_WebExProdToolsTestUsers_Install" \
-#						"ACL_SCCM_WebExProdTools_Install")
+# custom user-related conditions:
+# read list of groups from file and determin if the user is a member of the group
+# condition created is group_name == TRUE/FALSE
 
 DEFAULTS=/usr/bin/defaults
 MUNKI_DIR=$($DEFAULTS read /Library/Preferences/ManagedInstalls ManagedInstallDir)
 COND_DOMAIN="$MUNKI_DIR/ConditionalItems"
-GROUPFILE="$MUNKI_DIR/Group_List"
+GROUP_LIST="$MUNKI_DIR/Group_List"
 
+if [ ! -f "$GROUP_LIST" ]; then
+	touch "$GROUP_LIST"
+	#no point continuing at this stage is the group list file wasn't there to begin with
+	exit 0
+fi
 # determine the current console user
 CONSOLE_USER=$(who | grep console | cut -d ' ' -f1)
-
-# determine the main user of the machine
-AC_RESULT=$(ac -p | awk '!/total/' | sort -rnk 2 | head -n 1)
-MOST_USER=$(echo "$AC_RESULT" | awk '{print $1}')
 
 # run test based on the current console user - if no console user, take the user with the most time on the machine.
 if [[ $CONSOLE_USER != "" ]]; then
 	USER_ACC=$CONSOLE_USER
 else
-	USER_ACC=$MOST_USER
+	# Console user failed - determine the main user of the machine
+	AC_RESULT=$(ac -p | awk '!/total/' | sort -rnk 2 | head -n 1)
+	USER_ACC=$(echo "$AC_RESULT" | awk '{print $1}')¬  
 fi
 
 # determine is user is included in the right group
-
 while read LINE; do
 	GROUP_MEMBERSHIP=$(dsmemberutil checkmembership -U "$USER_ACC" -G "$LINE")
 
 	if [[ $GROUP_MEMBERSHIP == "user is a member of the group" ]]; then
-		#exit 0
-		$DEFAULTS write "$COND_DOMAIN" "$LINE" -bool TRUE
+		IS_MEMBER="TRUE"
 	else
-		#exit 1
-		$DEFAULTS write "$COND_DOMAIN" "$LINE" -bool FALSE
+		IS_MEMBER="FALSE"
 	fi
-done < "$GROUPFILE"
+
+	$DEFAULTS write ""$COND_DOMAIN" "$LINE" -bool $IS_MEMBER
+
+done < "$GROUP_LIST"
 
 exit 0
